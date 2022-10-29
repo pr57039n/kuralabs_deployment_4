24331e38924a2e0139ae790444e66d60bae98b18
pr57039n <107935666+pr57039n@users.noreply.github.com>
2022-10-29 06:40:18 -0400

    Update main.tf

--- intTerraform/main.tf
+++ intTerraform/main.tf
@@ -8,6 +8,25 @@ provider "aws" {
   
 }
 
+data "aws_availability_zones" "azs" {}
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = var.name
+  cidr = var.vpc_cidr
+
+  azs             = data.aws_availability_zones.azs.names
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  tags = {
+    Name = "${var.name}"
+  }
+}
 resource "aws_instance" "web_server01" {
   ami = "ami-08c40ec9ead489470"
   instance_type = "t2.micro"
